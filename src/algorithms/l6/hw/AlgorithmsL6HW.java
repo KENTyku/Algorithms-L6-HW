b32975a44008dd93cb0eec25f5ca2e69e709bd12
kentyku <kentyku@box>
2018-05-14 22:05:35 +0400

    Начало разработки.

---
+++ src/algorithms/l6/hw/AlgorithmsL6HW.java
@@ -0,0 +1,20 @@
+/*
+ * Use and copying for commercial purposes
+ * only with the author's permission
+ */
+package algorithms.l6.hw;
+
+/**
+ *
+ * @author kentyku
+ */
+public class AlgorithmsL6HW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
